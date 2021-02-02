494c02e561e9d48c56501a4ada2336f67a01d82e
Hashininirasha <hashininirashamunasinghe@gmail.com>
2021-02-02 10:32:42 +0530

    added smaile face

--- face.py
+++ face.py
@@ -2,6 +2,7 @@ import cv2
 
 cap = cv2.VideoCapture(0)
 cascade=cv2.CascadeClassifier("im/haarcascade_frontalface_default.xml")
+smilecascade=cv2.CascadeClassifier("im/haarcascade_smile.xml")
 while True:
     success,frame=cap.read()
     gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
@@ -9,6 +10,9 @@ while True:
 
     for (x,y,w,h) in faces:
         cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),3)
+        smile=smilecascade.detectMultiScale(gray,1.8,5)
+        for (x,y,w,h) in smile:
+            img=cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),3)
         cv2.imshow("My Face",frame)
         if cv2.waitKey(1) & 0xFF == ord("s"):
             break
